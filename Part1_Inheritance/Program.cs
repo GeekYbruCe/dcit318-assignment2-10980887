72254a606c6ab531f023d0aea51c0e006cdc9760
Lartey Solomon <93642216+GeekYbruCe@users.noreply.github.com>
2024-06-17 22:27:01 +0000

    Add Inheritance and Method Overriding part

---
+++ Part1_Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOP_Inheritance
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Snake : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Hiss");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Snake snake = new Snake();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound();
+            snake.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
